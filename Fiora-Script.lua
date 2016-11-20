5b482eef2e2758b14ebf8ddfa0809d8385fe1ab6
LordScripter <usercime@hotmail.be>
2016-11-20 15:56:16 +0100

    Update Fiora-Script.lua

--- Fiora-Script.lua
+++ Fiora-Script.lua
@@ -5,14 +5,12 @@ if GetObjectName(myHero) ~= "Fiora" then return end
 -- define variables
 local target
 local timerold,timernew, AAREADY,cAS = 0,0,0,0
-local usedQ, usedAA1, usedE, usedAA2, usedHydra, usedAA3 = 0,1,1,1,1,1
 -- perma refreshed
 OnTick(function(myHero)
 	if IOW:Mode() == "Combo"  then
 		combo()
-	else
-		usedQ, usedAA1, usedE, usedAA2, usedHydra, usedAA3 = 0,1,1,1,1,1
 	end
+	
 	cAS = GetAttackSpeed( GetMyHero() ) * 0.69
 	timernew = GetTickCount()
 	lastAA = timerold - timernew
@@ -25,45 +23,22 @@ end)
 function combo()
 target = GetCurrentTarget()
 
-			if usedQ == 0 then
-				castQ()
-				usedQ = 1
-				usedAA1 = 0
-				return
-			end
+			castQ()
 			
-			if AAREADY == 1 and usedAA1 == 0 then
+			if (AAREADY == 1 ) and CanUseSpell(myHero,_Q) ~= READY then
 				AttackUnit(target)
-				usedAA1 = 1
-				usedE = 0
-				return
 			end
 			
-			if AAREADY == 0 and usedE == 0 then
+			if AAREADY == 0 then
 				castE()
-				usedE = 1
-				usedAA2 = 0
-				return
 			end
 			
-			if AAREADY == 1 and usedAA2 == 0 then
-				AttackUnit(target)
-				usedAA2 = 1
-				usedHydra = 0
-				return
-			end
-	
-			if usedHydra == 0 then
-				CastOffensiveItems(target)
-				usedHydra = 1
-				usedAA3 = 0
-				return
+			if CanUseSpell(myHero,_E) ~= READY then
+			CastOffensiveItems(target)
 			end
 			
-			if AAREADY == 1 and usedAA3 == 0 then
+			if (AAREADY == 1 ) and CanUseSpell(myHero,_E) ~= READY then
 				AttackUnit(target)
-				usedAA3 = 1
-				return
 			end
 
 		end
@@ -88,4 +63,3 @@ OnProcessSpellComplete(function(Object,Spell)
 end)
 
 PrintChat("simple Fiora Combo loaded.")
-
