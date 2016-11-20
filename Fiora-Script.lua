fabb87b0532916d8ccc492d215ad879f0e3d495f
LordScripter <usercime@hotmail.be>
2016-11-20 23:03:40 +0100

    Update Fiora-Script.lua

--- Fiora-Script.lua
+++ Fiora-Script.lua
@@ -2,12 +2,35 @@
 
 local myHero = GetMyHero()
 if GetObjectName(myHero) ~= "Fiora" then return end
+-- import antiCC
+require 'antiCC'
+addAntiCCCallback(function(unit, spellProc)
+	if autoBlock.getValue() or combo.getValue() then
+		if CanUseSpell(myHero,_W) == READY then
+			CastSkillShot(_W,GetOrigin(unit))
+		end
+	end
+end)
 -- define variables
 local target
 local timerold,timernew, AAREADY,cAS = 0,0,0,0
+-- menu
+if not FileExist(COMMON_PATH.. "Analytics.lua") then
+  DownloadFileAsync("https://raw.githubusercontent.com/LoggeL/GoS/master/Analytics.lua", COMMON_PATH .. "Analytics.lua", function() end)
+end
+
+require("Analytics")
+
+Analytics("Eternal Akali", "Toshibiotro")
+
+local fioraMenu = Menu("Fiora","Fiora")
+fioraMenu:SubMenu("Combo","Combo")
+fioraMenu.Combo:Boolean("doCombo","doCombo", true)
+fioraMenu:SubMenu("AntiCC","AntiCC")
+fioraMenu.AntiCC:Boolean("AntiCC","AntiCC",true)
 -- perma refreshed
 OnTick(function(myHero)
-	if IOW:Mode() == "Combo"  then
+	if IOW:Mode() == "Combo" and doCombo == true then
 		combo()
 	end
 	
